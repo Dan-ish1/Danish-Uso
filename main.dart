50aa4642ff9013748bf0911a6399946a8d10e339
Dan-ish1 <danish.sleek1@gmail.com>
2023-06-11 17:27:35 +0500

    first commit

---
+++ main.dart
@@ -0,0 +1,11 @@
+void main() {
+  var name = "Danish";
+  var password = "Danish123";
+
+  if(name == "Danish" && password == "Danish123") {
+    print("Login succesful");
+  } else {
+    if (name != "Danish" && password != "Danish123") {
+  }
+  }
+  }
